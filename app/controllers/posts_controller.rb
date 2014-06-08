57a90f2ebe170a9ab1221af53a0cba8885845156
Tennin Hsu <tennin@fundky.com>
2014-06-08 21:25:14 +0800

    2-2 done associate categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,53 @@
 class PostsController < ApplicationController
-  def index
 
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.creator = User.first
+
+    if @post.save
+      flash[:notice] = 'Your post was created.'
+      redirect_to posts_path
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    binding.pry
+    if @post.update(post_params)
+      flash[:notice] = 'The post was updated!'
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description, category_ids: [])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531162713) do
+ActiveRecord::Schema.define(version: 20140602143330) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20140531162713) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer "post_id"
+    t.integer "category_id"
   end
 
   create_table "posts", force: true do |t|
